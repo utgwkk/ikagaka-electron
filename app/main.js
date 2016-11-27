d494732293ceaee2465ffc12342d78a85c2fc744
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-28 01:19:55 +0900

    %s/var/const/g

--- app/main.js
+++ app/main.js
@@ -6,26 +6,26 @@ Promise.all([
   NarLoader.loadFromURL("./nar/mobilemaster.nar"),
   NarLoader.loadFromURL("./nar/origin.nar")
 ]).then(function(arg){
-  var shellNanikaDir = arg[0]
-  var balloonNanikaDir = arg[1]
+  const shellNanikaDir = arg[0]
+  const balloonNanikaDir = arg[1]
   console.log(arg)
-  var shellDir = shellNanikaDir.getDirectory("shell/master").asArrayBuffer();
-  var balloonDir = balloonNanikaDir.asArrayBuffer();
-  var shell = new cuttlebone.Shell(shellDir);
-  var balloon = new cuttlebone.Balloon(balloonDir);
+  const shellDir = shellNanikaDir.getDirectory("shell/master").asArrayBuffer();
+  const balloonDir = balloonNanikaDir.asArrayBuffer();
+  const shell = new cuttlebone.Shell(shellDir);
+  const balloon = new cuttlebone.Balloon(balloonDir);
   return Promise.all([
     shell.load(),
     balloon.load()
   ]);
 }).then(function(arg){
-  var shell = arg[0];
-  var balloon = arg[1];
+  const shell = arg[0];
+  const balloon = arg[1];
 
-  var nmdmgr = new cuttlebone.NamedManager()
+  const nmdmgr = new cuttlebone.NamedManager()
   document.body.appendChild(nmdmgr.element);
 
-  var hwnd = nmdmgr.materialize(shell, balloon);
-  var named = nmdmgr.named(hwnd);
+  const hwnd = nmdmgr.materialize(shell, balloon);
+  const named = nmdmgr.named(hwnd);
 
   named.scope(0).surface(0);
   named.scope(0).blimp(0);
