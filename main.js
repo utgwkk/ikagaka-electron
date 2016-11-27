c69209088e7749b55496a178ef3c502d60b826b8
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-28 04:35:39 +0900

    Add debug mode and transparency

--- main.js
+++ main.js
@@ -8,16 +8,33 @@ const url = require('url')
 let mainWindow
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  const size = electron.screen.getPrimaryDisplay().size
+
+  const debug = true;
+  if (debug) {
+    mainWindow = new BrowserWindow({width: 1024, height: 768})
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width: size.width,
+      height: size.height,
+      show: true,
+      frame: false,
+      transparent: true,
+      alwaysOnTop: true
+    })
+    mainWindow.maximize()
+    mainWindow.setIgnoreMouseEvents(true)
+  }
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
